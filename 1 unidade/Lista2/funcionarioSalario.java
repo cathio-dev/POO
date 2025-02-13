9f7e6df5d3d2dcb7e4c46404eaceee011c5667b4
cathio-dev <ramalho.marcos7@gmail.com>
2025-02-13 07:40:16 -0300

    feat - atv func

--- 1 unidade/Lista2/funcionarioSalario.java
+++ 1 unidade/Lista2/funcionarioSalario.java
@@ -7,7 +7,29 @@ public class funcionarioSalario {
             Locale.setDefault(Locale.US);
             Scanner sc = new Scanner(System.in);
 
-            
+            String nome;
+            double salBruto = 0, imposto, percentual = 0, salLiq = 0, salImposto;
+
+            System.out.print("Nome: ");
+            nome = sc.nextLine();
+
+            System.out.print("Salario bruto: ");
+            salBruto = sc.nextDouble();
+
+            System.out.print("Imposto: ");
+            imposto = sc.nextDouble();
+
+            salImposto = salBruto - imposto;
+            System.out.println();
+
+            System.out.println("Empregado: " + nome + ", R$ " + String.format("%.2f", salImposto));
+            System.out.println();
+
+            System.out.print("Qual o percentual de aumento do salário?: ");
+            percentual = sc.nextDouble();
+            salLiq = salImposto + (salBruto * 0.1);
+
+            System.out.printf("Dados atualizados: %s, R$ %.2f", nome, salLiq);
             
             
         sc.close();
