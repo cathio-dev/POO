0980c71bd4d694a017edfb37781bafd3c1ea3f7a
cathio-dev <ramalho.marcos7@gmail.com>
2025-02-20 08:40:04 -0300

    Terminar aluno.java e programa.java

---
+++ 1 unidade/PrimeiroExemplo/src/Entidades/Aluno.java
@@ -0,0 +1,15 @@
+package Entidades;
+
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private float mediaGeral;
+
+    public Aluno(int matricula, String nome, float mediaGeral){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.mediaGeral = mediaGeral;
+    }
+
+    
+}
