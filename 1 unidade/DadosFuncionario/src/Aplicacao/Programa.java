52e8582d04d88443a47c3765cda4dd866cacb4f8
cathio-dev <ramalho.marcos7@gmail.com>
2025-02-23 23:04:38 -0300

    Dados Funcionario

---
+++ 1 unidade/DadosFuncionario/src/Aplicacao/Programa.java
@@ -0,0 +1,44 @@
+package Aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entidades.Funcionario;
+
+public class Programa {
+    public static void main(String[] args) throws Exception {
+        Locale ptBR = new Locale("pt", "BR");
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        float salBruto, imposto;
+
+        System.out.print("Nome: ");
+        nome = sc.nextLine();
+
+        System.out.print("Salário Bruto: ");
+        salBruto = sc.nextFloat();
+
+        System.out.print("Imposto: ");
+        imposto = sc.nextFloat();
+
+        Funcionario funcionario = new Funcionario(nome, salBruto, imposto);
+        funcionario.Imposto(imposto);
+
+        System.out.println();
+
+        System.out.printf("Empregado: %s, R$ %.2f\n", funcionario.getNome(), funcionario.getSalBruto());
+        System.out.println();
+
+        System.out.print("Qual o percentual de aumento do salário?: ");
+        float aumento = sc.nextFloat();
+
+        funcionario.percentualAumento(aumento);
+
+        System.out.println();
+
+        System.out.printf("Dados Atualizados: %s, R$ %.2f", funcionario.getNome(), funcionario.getSalBruto());
+
+        sc.close();
+    }
+}
---
+++ 1 unidade/DadosFuncionario/src/Entidades/Funcionario.java
@@ -0,0 +1,34 @@
+package Entidades;
+
+public class Funcionario {
+    private String nome;
+    private float salBruto;
+
+    public Funcionario(String nome, float salBruto, float imposto) {
+        this.nome = nome;
+        this.salBruto = salBruto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getSalBruto() {
+        return salBruto;
+    }
+    public void setSalBruto(float salBruto) {
+        this.salBruto = salBruto;
+    }
+
+    public void Imposto(float imposto){
+        this.salBruto = this.salBruto - imposto;
+    }
+    
+    public void percentualAumento(float percentual){
+        this.salBruto = this.salBruto + ( (percentual/100) * this.salBruto );
+    }
+    
+    
+}
